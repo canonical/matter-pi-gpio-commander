025b18271f43bdfe3c02d62057a77a368946d5a7
Lincoln Wallace <lincoln.wallace@canonical.com>
2024-02-06 14:06:39 -0300

    add: script to setup gpio-mockup
    Signed-off-by: Lincoln Wallace <lincoln.wallace@canonical.com>

---
+++ tests/gpio-mock.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+mkdir gpio-mockup
+cd gpio-mockup
+
+# Update and install dependencies
+sudo apt-get update
+sudo apt-get install -y linux-headers-$(uname -r)
+sudo apt-get install -y build-essential flex bison make
+
+kernel_major_minor=$(uname -r | cut -d'.' -f1-2)
+
+echo "Kernel major minor version: $kernel_major_minor"
+
+# Get GPIO Mockup driver
+wget https://raw.githubusercontent.com/torvalds/linux/v$kernel_major_minor/drivers/gpio/gpio-mockup.c
+wget https://raw.githubusercontent.com/torvalds/linux/v$kernel_major_minor/drivers/gpio/gpiolib.h
+
+# Create Makefile
+echo "
+obj-m = gpio-mockup.o
+KVERSION = \$(shell uname -r)
+all:
+	make -C /lib/modules/\$(KVERSION)/build M=\$(PWD) modules
+clean:
+	make -C /lib/modules/\$(KVERSION)/build M=\$(PWD) clean
+" > Makefile
+
+
+make -j$(nproc)
+
+sudo insmod gpio-mockup.ko gpio_mockup_ranges=-1,16
+
+gpio_mock_chip=$(ls /dev/gpiochip* | sort -n | head -n 1)
+
+echo "GPIO Mockup chip: $gpio_mock_chip"
+
+cd ..
