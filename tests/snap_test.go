06e443f3cc0ccc4a52baf7ed47855778462228d1
Lincoln Wallace <lincoln.wallace@canonical.com>
2024-02-08 21:18:32 +0000

    chore: local test for test-blink application
    Signed-off-by: Lincoln Wallace <lincoln.wallace@canonical.com>

--- tests/snap_test.go
+++ tests/snap_test.go
@@ -2,22 +2,37 @@ package tests
 
 import (
 	"context"
-	"errors"
-	"fmt"
 	"log"
 	"os"
 	"testing"
 	"time"
 
 	"github.com/canonical/matter-snap-testing/utils"
+	"github.com/stretchr/testify/assert"
 )
+
+const (
+	// enviroment variables
+	SpecificGpioChip = "GPIO_CHIP"
+	SpecifGpioLine   = "GPIO_LINE"
+)
+
+var (
+	TestedGpioChip = ""
+	TestedGpioLine = ""
+)
+
+func init() {
+	TestedGpioChip = os.Getenv(SpecificGpioChip)
+	TestedGpioLine = os.Getenv(SpecifGpioLine)
+}
 
 var start = time.Now()
 
 const snapName = "matter-pi-gpio-commander"
 
 func TestMain(m *testing.M) {
-	teardown, err := setup()
+	teardown, err := setup() // THIS NEED TO receive "t" but this function doen't have access to it TODO: fix this latter
 	if err != nil {
 		log.Fatalf("Failed to setup tests: %s", err)
 	}
@@ -56,50 +71,50 @@ func setup() (teardown func(), err error) {
 	}
 
 	// connect interfaces
-	utils.SnapConnect(nil, snapName+":avahi-observe", "")
+	utils.SnapConnect(nil, snapName+":avahi-control", "")
 	utils.SnapConnect(nil, snapName+":bluez", "")
-	utils.SnapConnect(nil, snapName+":process-control", "")
+	utils.SnapConnect(nil, snapName+":network", "")
+	utils.SnapConnect(nil, snapName+":network-bind", "")
 	utils.SnapConnect(nil, snapName+":custom-gpio", snapName+":custom-gpio-dev")
 	return
 }
 
-func getMockGPIO(t *testing.T) (string, error) {
+func getMockGPIO(t *testing.T) string {
+	t.Helper()
 	gpioChipNumber, stderr, err := utils.Exec(t, "ls /dev/gpiochip* | sort -n | tail -n 1 | cut -d'p' -f3")
 	if err != nil || stderr != "" {
-		msg := fmt.Sprintf("ERROR %s: %s", err, stderr)
-		return "", errors.New(msg)
+		t.Fatalf("Failed to get mock gpio chip number, Error %s: %s", stderr, err)
 	}
-	return gpioChipNumber, nil
+	return gpioChipNumber
 }
 
 func TestBlinkOperation(t *testing.T) {
-
-	gpiochip, err := getMockGPIO(t)
-	if err != nil {
-		t.Fatal("Error when getting the gpio-mockup chip number: %s", err)
-	}
-
 	log.Println("[TEST] Standard blink operation")
-	utils.SnapSet(t, snapName, "gpiochip", gpiochip)
-	utils.SnapSet(t, snapName, "gpio", "4")
 
-	// Run the context in background
-	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Minute) // Adjust the timeout as needed
-	defer cancel()
-
-	go func() {
-		utils.Exec(t, snapName+".test-blink")
-		cancel()
-	}()
-
-	<-ctx.Done()
-
-	utils.Exec(t, snapName+".test-blink")
-	// Check if the context was done due to a timeout or because Exec returned
-	if ctx.Err() == context.DeadlineExceeded {
-		t.Log("Test did not complete within the expected time frame.")
-		t.FailNow()
-	} else {
-		t.Log("Test completed within the expected time frame.")
+	gpiochip := TestedGpioChip
+	if gpiochip == "" {
+		gpiochip = getMockGPIO(t)
+		log.Printf("[TEST] No specific gpiochip defined, using mockup gpio: /dev/gpiochip%s", gpiochip)
 	}
+
+	gpioline := TestedGpioLine
+	if gpioline == "" {
+		gpioline = "4"
+		log.Printf("[TEST] No specific gpio line defined, using default: %s", gpioline)
+	}
+
+	utils.SnapSet(t, snapName, "gpiochip", gpiochip)
+	utils.SnapSet(t, snapName, "gpio", gpioline)
+
+	// test blink operation
+
+	t.Run("test-blink", func(t *testing.T) {
+		ctx, cancel := context.WithDeadline(context.Background(), <-time.After(10*time.Second))
+		defer cancel()
+
+		_, _, err := utils.ExecContextVerbose(nil, ctx, snapName+".test-blink")
+		t.Logf("err: %s", err)
+		assert.Error(t, err, "Expected an error")
+		assert.Equal(t, "context deadline exceeded", err.Error())
+	})
 }
