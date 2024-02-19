b9ee23797d3fd743476bf8081b32ad8e64e1de03
Lincoln Wallace <lincoln.wallace@canonical.com>
2024-02-19 20:25:27 +0000

    fix: usage of exec.CommandContext
    Signed-off-by: Lincoln Wallace <lincoln.wallace@canonical.com>

--- tests/snap_test.go
+++ tests/snap_test.go
@@ -200,7 +200,7 @@ func TestBlinkOperation(t *testing.T) {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	t.Cleanup(cancel)
 
-	cmd := exec.CommandContext(ctx, "sudo "+snapMatterPiGPIO+".test-blink")
+	cmd := exec.CommandContext(ctx, "sudo", snapMatterPiGPIO+".test-blink")
 	cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
 
 	stdoutPipe, err := cmd.StdoutPipe()
