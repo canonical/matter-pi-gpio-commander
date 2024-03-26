54bb1be59f2153ddff896b9985e12847abab1460
Lincoln Wallace <lincoln.wallace@canonical.com>
2024-03-26 15:28:31 -0300

    feat: solve libgpiod API problem compiling lib from source
    Signed-off-by: Lincoln Wallace <lincoln.wallace@canonical.com>

--- test-blink.cpp
+++ test-blink.cpp
@@ -1,5 +1,8 @@
 // Build:
+// Dynamically
 // g++ -Wall test-blink.cpp -lgpiod -o test-blink
+// Statically (Used in this snap)
+// g++ -Wall -I<path to gpiod.h dir> -L<path to libgpiod.a dir> test-blink.cpp -lgpiod -static -o test-bin
 
 #include <gpiod.h>
 #include <cstdlib>
@@ -13,17 +16,73 @@
 
 #define GPIO_CONSUMER "matter-commander-test-blink"
 
-void setGpioLineValue(struct gpiod_line *line, int value);
+static struct gpiod_line_request *
+requestOutputGPIOline(std::string *chip_path, unsigned int offset,
+		    enum gpiod_line_value value)
+{
+	struct gpiod_request_config *req_cfg = NULL;
+	struct gpiod_line_request *request = NULL;
+	struct gpiod_line_settings *settings;
+	struct gpiod_line_config *line_cfg;
+	struct gpiod_chip *chip;
+	int ret;
+
+	chip = gpiod_chip_open(chip_path->c_str());
+	if (!chip)
+		return NULL;
+
+	settings = gpiod_line_settings_new();
+	if (!settings){
+        std::cerr << "Error: not possible to config new line, closing chip" 
+                      << std::endl;
+	    gpiod_chip_close(chip);
+    }
+
+	gpiod_line_settings_set_direction(settings,
+					  GPIOD_LINE_DIRECTION_OUTPUT);
+	gpiod_line_settings_set_output_value(settings, value);
+
+	line_cfg = gpiod_line_config_new();
+	if (!line_cfg) {
+        std::cerr <<  "Error: not possible to config new line, freeing setings" 
+                          << std::endl;
+	    gpiod_line_settings_free(settings);
+    }
+ 
+	ret = gpiod_line_config_add_line_settings(line_cfg, &offset, 1,
+						  settings);
+
+	req_cfg = gpiod_request_config_new();
+	if (!req_cfg || ret){
+        std::cerr << "Error: Not possible to request new config. Freeing line \
+                          config" << std::endl;
+	    gpiod_line_config_free(line_cfg);
+    }
+
+	gpiod_request_config_set_consumer(req_cfg, GPIO_CONSUMER);
+
+	request = gpiod_chip_request_lines(chip, req_cfg, line_cfg);
+
+	gpiod_request_config_free(req_cfg);
+
+	return request;
+}
+
+static enum gpiod_line_value toggle_line_value(enum gpiod_line_value value)
+{
+	return (value == GPIOD_LINE_VALUE_ACTIVE) ? GPIOD_LINE_VALUE_INACTIVE :
+						    GPIOD_LINE_VALUE_ACTIVE;
+}
 
 int main(void)
 {
-    struct gpiod_line *line;
 
     // Read environment variables
     char *envGPIO = std::getenv(GPIO);
     if (envGPIO == NULL || strlen(envGPIO) == 0)
     {
-        std::cout << "Unset or empty environment variable: " << GPIO << std::endl;
+        std::cout << "Unset or empty environment variable: " << GPIO 
+            << std::endl;
         return 1;
     }
     std::cout << "GPIO: " << envGPIO << std::endl;
@@ -31,16 +90,16 @@ int main(void)
     char *envGPIOCHIP = std::getenv(GPIOCHIP);
     if (envGPIOCHIP == NULL || strlen(envGPIOCHIP) == 0)
     {
-        std::cout << "Unset or empty environment variable: " << GPIOCHIP << std::endl;
+        std::cout << "Unset or empty environment variable: " << GPIOCHIP 
+            << std::endl;
         return 1;
     }
     std::cout << "GPIOCHIP: " << envGPIOCHIP << std::endl;
 
-    // Convert
-    int gpio;
+    int gpioLine;
     try
     {
-        gpio = std::stoi(envGPIO);
+        gpioLine = std::stoi(envGPIO);
     }
     catch (std::exception &ex)
     {
@@ -50,49 +109,29 @@ int main(void)
 
     std::string gpioDevice = (std::string)"/dev/gpiochip" + envGPIOCHIP;
     
-    // Setup GPIO with libgpiod
-    struct gpiod_chip *chip;
 
-    chip = gpiod_chip_open(gpioDevice.c_str());
-    if (!chip)
-    {
-        std::cerr << "Failed to open gpio chip: " << gpioDevice << std::endl;
+    struct gpiod_line_request *request;
+    enum gpiod_line_value value = GPIOD_LINE_VALUE_ACTIVE; 
+
+    request = requestOutputGPIOline(&gpioDevice, gpioLine, value);
+    if (!request) {
+        std::cerr << "Failed to request output line.\n"<< std::strerror(errno) 
+            << std::endl;
         return 1;
     }
 
-    line = gpiod_chip_get_line(chip, gpio);
-    if (!line)
-    {
-        std::cerr << "Failed to get gpio line: " << gpio << std::endl;
-        return 1;
-    }
-
-    int ret = gpiod_line_request_output(line, GPIO_CONSUMER, 0);
-    if (ret < 0)
-    {
-        std::cerr << "Failed to set gpio line as output." << std::endl;
-        return 1;
-    }
-
+    std::string msg;
     for (;;)
     {
-        setGpioLineValue(line, 1);
-        std::cout << "On" << std::endl;
-        usleep(5e5);
 
-        setGpioLineValue(line, 0);
-        std::cout << "Off" << std::endl;
-        usleep(5e5);
+        value = toggle_line_value(value);
+        std::cout << "Setting GPIO " + std::to_string(gpioLine) + " to " + 
+            ((value == GPIOD_LINE_VALUE_ACTIVE)? "On" : "Off") << std::endl;
+        gpiod_line_request_set_value(request, gpioLine, value);
+        usleep(5e5); 
     }
 
-    return 0;
-}
+    gpiod_line_request_release(request);
 
-void setGpioLineValue(struct gpiod_line *line, int value)
-{
-   int ret = gpiod_line_set_value(line, value);
-   if(ret < 0)
-   {
-        std::cerr << "Failed to set gpio line to " << value << std::endl;
-   }
+    return 0;
 }
